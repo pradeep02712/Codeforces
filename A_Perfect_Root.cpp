154726c50f83fc00466ed3b27c1f395fb555b818
Pradeep Singh <mrbrute27@gmail.com>
2026-08-29 19:49:27 +0530

    Add files via upload

---
+++ A_Perfect_Root.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+void solve(){
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<i+1<<" ";
+       
+    }
+    cout<<endl;
+
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t= 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
