40802639c7b5cb1005e86f4140a5e6f178e03994
Pradeep Singh <mrbrute27@gmail.com>
2026-09-02 21:46:35 +0530

    Add files via upload

---
+++ A_Convergence.cpp
@@ -0,0 +1,29 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ 
+ #define int long long
+ #define endl '\n'
+ 
+ void solve(){
+ 
+     int n;
+     vector<int> a(n);
+     for(int i=0;i<n ; i++){
+        cin >> a[i];
+     }
+     sort(a.begin(),a.end());
+      
+ }
+ 
+ signed main(){
+ 
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     cout.tie(0);
+ 
+     int t= 1;
+     //cin >> t;
+     while(t--){
+         solve();
+     }
+ }
