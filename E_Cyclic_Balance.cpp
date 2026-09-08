9e301f42afcf88570e983eda1038d7d0bf3cbba1
Pradeep Singh <mrbrute27@gmail.com>
2026-09-08 22:10:19 +0530

    Add files via upload

---
+++ E_Cyclic_Balance.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+// pairs
+typedef pair<int, int> pint;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdb;
+
+// vectors
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<double> vdb;
+
+// vectors of vectors
+typedef vector<vint> vvint;
+typedef vector<vll> vvll;
+typedef vector<vdb> vvdb;
+
+#define frlp(v) for (auto ele : v) 
+#define frlpr(v) for (auto& ele : v) 
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++) 
+#define rrep(i, n) for (int i = (int)(n) - 1; i >= 0; i--) 
+#define reprng(i, s, e) for (int i = (int)(s); i < (int)(e); i++) 
+#define rreprng(i, e, s) for (int i = (int)(e); i >= (int)(s); i--) 
+
+#define pb push_back 
+#define qb pop_back 
+#define pf push_front 
+#define qf pop_front 
+
+#define maxe max_element 
+#define mine min_element 
+
+#define lmnt(x) x[(x).size() - 1] 
+
+#define yn(ans) printf("%s\n", (ans) ? "yes" : "no"); 
+#define YN(ans) printf("%s\n", (ans) ? "YES" : "NO"); 
+
+const ll INF = 1e18;
+
+template<typename T>
+bool chmax(T &a, const T b) { 
+    if (a >= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+bool chmin(T &a, const T b) { 
+    if (a <= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+void print(vector<T>& v, bool withSize = false) { 
+    if (withSize) cout << v.size() << endl;
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+void solve(){
+    int n,q;
+    cin >>n>>q;
+    string s;
+    cin >> s;
+    vint a(n),b(n),c(n),d(n);
+    reprng(i,1,n){
+        a[i] = a[i - 1];
+        b[i] = b[i - 1];
+        c[i] = c[i - 1];
+        d[i] = d[i - 1];
+        if (s[i - 1] == '0' && s[i] == '0') a[i]++;
+        if (s[i - 1] == '0' && s[i] == '1') b[i]++;
+        if (s[i - 1] == '1' && s[i] == '0') c[i]++;
+        if (s[i - 1] == '1' && s[i] == '1') d[i]++;
+    }
+    while (q--){
+        int l,r;
+        cin >> l >> r;
+        l--;
+        r--;
+        ll x = a[r] - a[l];
+        ll y = b[r] - b[l];
+        ll z = d[r] - d[l];
+        if (s[r] == '0' && s[l] == '0') x++;
+        if (s[r] == '0' && s[l] == '1') y++;
+        if (s[r] == '1' && s[l] == '1') z++;
+        ll k = y;
+        chmax(k, (x + y + 1) / 2);
+        chmax(k, (z + y + 1) / 2);
+        chmax(k, (x + y + z + 2) / 3);
+        cout << 4 * k - (r - l + 1) << endl;
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test1=1;
+    //cin >> test1;
+
+    while (test1--) {
+        solve();
+    }
+
+    return 0;
+}
