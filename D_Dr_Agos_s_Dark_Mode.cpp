580e8ef3063a0f3b114d281eb4168407d4341708
Pradeep Singh <mrbrute27@gmail.com>
2026-09-14 14:30:26 +0530

    Add files via upload

---
+++ D_Dr_Agos_s_Dark_Mode.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+// pairs
+typedef pair<int, int> pint;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdb;
+
+// vectors
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<double> vdb;
+
+// vectors of vectors
+typedef vector<vint> vvint;
+typedef vector<vll> vvll;
+typedef vector<vdb> vvdb;
+
+#define frlp(v) for (auto ele : v) 
+#define frlpr(v) for (auto& ele : v) 
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++) 
+#define rrep(i, n) for (int i = (int)(n) - 1; i >= 0; i--) 
+#define reprng(i, s, e) for (int i = (int)(s); i < (int)(e); i++) 
+#define rreprng(i, e, s) for (int i = (int)(e); i >= (int)(s); i--) 
+
+#define pb push_back 
+#define qb pop_back 
+#define pf push_front 
+#define qf pop_front 
+
+#define maxe max_element 
+#define mine min_element 
+
+#define lmnt(x) x[(x).size() - 1] 
+
+#define yn(ans) printf("%s\n", (ans) ? "yes" : "no"); 
+#define YN(ans) printf("%s\n", (ans) ? "YES" : "NO"); 
+
+const ll INF = 1e18;
+
+template<typename T>
+bool chmax(T &a, const T b) { 
+    if (a >= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+bool chmin(T &a, const T b) { 
+    if (a <= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+void print(vector<T>& v, bool withSize = false) { 
+    if (withSize) cout << v.size() << endl;
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+void solve(){
+    int n;
+    cin >> n;
+    string s(n,'0');
+    if (n == 1){
+        cout << "1" << endl;
+        return;
+    }
+    if (n == 2){
+        cout << "11" << endl;
+        return;
+    }
+    if (n%6 == 2){
+        s[0] = '1';
+        s[(n+4) / 3-1] = '1';
+        s[(2*n+2) / 3-1] = '1';
+    }
+    else{
+        int x = (n+1)/3;
+        int b = x+x%2;
+        int a = (n+1-b)/2;
+        s[a-1] = '1';
+        s[a+b-1] = '1';
+    }
+    cout << s << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test1=1;
+    cin >> test1;
+
+    while (test1--) {
+        solve();
+    }
+
+    return 0;
+}
