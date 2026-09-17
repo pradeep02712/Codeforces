3f09573fa8bae9d080bbcdb72992a67b0d245a58
Pradeep Singh <mrbrute27@gmail.com>
2026-09-17 22:05:23 +0530

    Add files via upload

---
+++ C_Tree_Cutting.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+// pairs
+typedef pair<int, int> pint;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdb;
+
+// vectors
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<double> vdb;
+
+// vectors of vectors
+typedef vector<vint> vvint;
+typedef vector<vll> vvll;
+typedef vector<vdb> vvdb;
+
+#define frlp(v) for (auto ele : v) 
+#define frlpr(v) for (auto& ele : v) 
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++) 
+#define rrep(i, n) for (int i = (int)(n) - 1; i >= 0; i--) 
+#define reprng(i, s, e) for (int i = (int)(s); i < (int)(e); i++) 
+#define rreprng(i, e, s) for (int i = (int)(e); i >= (int)(s); i--) 
+
+#define pb push_back 
+#define qb pop_back 
+#define pf push_front 
+#define qf pop_front 
+
+#define maxe max_element 
+#define mine min_element 
+
+#define lmnt(x) x[(x).size() - 1] 
+
+#define yn(ans) printf("%s\n", (ans) ? "yes" : "no"); 
+#define YN(ans) printf("%s\n", (ans) ? "YES" : "NO"); 
+
+const ll INF = 1e18;
+const int N = 1e5 + 69;
+template<typename T>
+bool chmax(T &a, const T b) { 
+    if (a >= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+bool chmin(T &a, const T b) { 
+    if (a <= b) return false;
+    a = b;
+    return true;
+}
+
+template<typename T>
+void print(vector<T>& v, bool withSize = false) { 
+    if (withSize) cout << v.size() << endl;
+    rep(i, v.size()) cout << v[i] << " ";
+    cout << endl;
+}
+
+int n, k, x;
+ll a[N], b[N];
+vint g[N];
+
+void dfs(int u, int p = -1) {
+    a[u] = 0;
+    b[u] = 1;
+    for (int v : g[u]) {
+        if (v == p) continue;
+        dfs(v, u);
+        a[u] += a[v];
+        b[u] += b[v];
+    }
+    if (b[u] >= x){
+        b[u] = 0;
+        a[u]++;
+    }
+}
+bool ok(int m){
+    x = m;
+    dfs(1);
+    return a[1] >= k + 1;
+}
+void solve(){
+    cin >> n >> k;
+    reprng(i,1,n+1){
+        g[i].clear();
+    }
+    rep(i,n-1){
+        int u,v;
+        cin >>u>>v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    int l = 1,r = n;
+    while (l < r){
+        int m = (l + r + 1) / 2;
+        if (ok(m))
+            l = m;
+        else
+            r = m - 1;
+    }
+    cout << l << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test1=1;
+    cin >> test1;
+
+    while (test1--) {
+        solve();
+    }
+
+    return 0;
+}
